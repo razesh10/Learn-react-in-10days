f794c6b26c35167bc8d79d01a6f718d3ed8cbcbc
razesh10 <razesh.leo19@gmail.com>
2023-12-13 16:42:51 +0545

    second-commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
+    <>
     <div className="App">
-      
+      <h1>Rajesh Chaudhary</h1>
       
     </div>
+    </>
   );
 }
 
